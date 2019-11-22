464a419da9c370e8b69cb594ab4efe4bb8eebf63
Dan Garcia <daniel.of@gmail.com>
2019-11-22 19:19:45 -0300

    Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
+  /** HOOK useState - Estados */
   // useState recebe o valor inicial
   // tech é o estado setTech a função que atualiza o estado
   const [techs, setTechs] = useState(['React', 'Node', 'React Native']);
@@ -11,6 +12,7 @@ function App() {
     setNewTech('');
   }
 
+  /** HOOK useEffect - ciclo de vida */
   // useEffect substitui as funções de ciclo de vida
   // utilizando useEffect como o componentDidMount
   // basta passar um array vazio
@@ -32,6 +34,10 @@ function App() {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
 
+  //* * HOOK useMemo - para calculos e manipulação dos dados */
+  // recebe uma função e os parametros que quando alterados executam a função
+  // retorna algo específico
+  const techsSize = useMemo(() => techs.length, [techs]);
   return (
     <>
       <ul>
@@ -39,6 +45,7 @@ function App() {
           <li key={tech}>{tech}</li>
         ))}
       </ul>
+      <strong>Você tem {techsSize} tecnologias</strong> <br />
       <input
         type="text"
         value={newTech}
